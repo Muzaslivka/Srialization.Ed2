e1b47692d8c9d01febe277a7e774af3c9c076e7f
muzaslivka <ms.dmitrieffa@gmail.com>
2023-04-01 21:37:19 +0300

    Initial commit

---
+++ src/Basket.java
@@ -0,0 +1,84 @@
+import java.io.*;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Basket {
+    private String[] products;
+    private int[] prices;
+    private int[] countProduct;
+
+    public Basket(String[] products, int[] prices) {
+        this.products = products;
+        this.prices = prices;
+        this.countProduct = new int[products.length];
+    }
+
+    public void addToCart(int productNum, int amount) {
+        countProduct[productNum - 1] += amount;
+    }
+
+    private int sumTotal() {
+        int sum = 0;
+        for (int j = 0; j < products.length; j++) {
+            if (countProduct[j] != 0) {
+                int sumProduct = prices[j] * countProduct[j];
+                sum += sumProduct;
+            }
+        }
+        return sum;
+    }
+
+    public void printCart() {
+        System.out.println("Ваша корзина:");
+        for (int i = 0; i < products.length; i++) {
+            if (countProduct[i] != 0) {
+                int sum = prices[i] * countProduct[i];
+                System.out.println(products[i] + " (" + prices[i] + " руб/шт) - " + countProduct[i] + " шт (Итого: " + sum + " руб)");
+            }
+        }
+        System.out.println("Итоговая сумма: " + sumTotal() + " руб");
+    }
+
+    public void printChoice() {
+        System.out.println("Список продуктов для выбора:");
+        for (int i = 0; i < products.length; i++) {
+            System.out.println((i + 1) + ". " + products[i] + " " + prices[i] + " руб/шт");
+        }
+    }
+
+    public void saveTxt(File textFile) throws IOException {
+        try (BufferedWriter outBuffer = new BufferedWriter(new FileWriter(textFile))) {
+            for (int i = 0; i < products.length; i++) {
+                outBuffer.write(products[i] + ";" + prices[i] + ";" + countProduct[i]);
+                outBuffer.newLine();
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static Basket loadFromTxtFile(File textFile) throws IOException {
+        Basket basket = null;
+        try (BufferedReader inBuffer = new BufferedReader(new FileReader(textFile))) {
+            List<String> fileInfo = inBuffer.lines().collect(Collectors.toList());
+            String[] fileProducts = new String[fileInfo.size()];
+            int[] filePrices = new int[fileInfo.size()];
+            int[] fileCount = new int[fileInfo.size()];
+            for (int i = 0; i < fileInfo.size(); i++) {
+                String[] fileLine = fileInfo.get(i).split(";");
+                fileProducts[i] = fileLine[0];
+                filePrices[i] = Integer.parseInt(fileLine[1]);
+                fileCount[i] = Integer.parseInt(fileLine[2]);
+            }
+            basket = new Basket(fileProducts, filePrices);
+            for (int j = 0; j < fileCount.length; j++) {
+                basket.addToCart(j + 1, fileCount[j]);
+            }
+            System.out.println("У вас уже есть список покупок");
+            basket.printCart();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return basket;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,77 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+
+    public static boolean isEmptyFile(File textFile) throws IOException {
+        try (BufferedReader inBuffer = new BufferedReader(new FileReader(textFile))) {
+            return inBuffer.readLine() == null;
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return false;
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        final String[] products = {"Молоко", "Хлеб", "Сливочное масло", "Макароны", "Творог", "Мука"};
+
+        final int[] prices = {80, 45, 120, 62, 88, 74};
+
+        Scanner sc = new Scanner(System.in);
+        File fileBasket = new File("basket.txt");
+        Basket basket;
+
+        if (fileBasket.canRead() && !isEmptyFile(fileBasket)) {
+            try {
+                basket = Basket.loadFromTxtFile(fileBasket);
+            } catch (Exception e) {
+                System.out.println("Неверный формат файла " + fileBasket.getName() + ". Будет создана пустая корзина");
+                basket = new Basket(products, prices);
+            }
+        } else {
+            System.out.println("У вас пустая корзина");
+            basket = new Basket(products, prices);
+        }
+
+        basket.printChoice();
+
+        while (true) {
+            System.out.println("Введите номер продукта и его количество через пробел или end для выхода:");
+            String input = sc.nextLine();
+            if (input.equals("end")) {
+                basket.printCart();
+                break;
+            }
+            String[] foodBasket = input.split(" ");
+            if (foodBasket.length != 2) {
+                System.out.println("Неверный ввод: введено не 2 значения. Попробуйте еще раз");
+            } else {
+                int numberProduct;
+                try {
+                    numberProduct = Integer.parseInt(foodBasket[0]);
+                } catch (NumberFormatException exception) {
+                    System.out.println("Вы ввели не число. Попробуйте еще раз");
+                    continue;
+                }
+                if (numberProduct > products.length || numberProduct <= 0) {
+                    System.out.println("Неверный номер продукта. Попробуйте еще раз");
+                } else {
+                    int count;
+                    try {
+                        count = Integer.parseInt(foodBasket[1]);
+                    } catch (NumberFormatException exception) {
+                        System.out.println("Вы ввели не число. Попробуйте еще раз");
+                        continue;
+                    }
+                    if (count < 0) {
+                        System.out.println("Введено отрицательное количество. Попробуйте еще раз");
+                    } else {
+                        basket.addToCart(numberProduct, count);
+                        basket.saveTxt(fileBasket);
+                    }
+                }
+            }
+        }
+    }
+}
