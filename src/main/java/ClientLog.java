59a679319a80d5b0373e04e81f4340986a5b280c
muzaslivka <ms.dmitrieffa@gmail.com>
2023-04-01 22:09:38 +0300

    shop.xml updated

--- src/main/java/ClientLog.java
+++ src/main/java/ClientLog.java
@@ -2,7 +2,7 @@ import com.opencsv.CSVWriter;
 
 import java.io.File;
 import java.io.FileWriter;
-import java.io.IOException;
+import java.io.IOException;git status
 import java.util.ArrayList;
 import java.util.List;
 
